1dfe92e98b07fc394fae0d15b24ccc8f4670c54f
meerkat-morecats <kit_hack@outlook.com>
2021-06-01 19:12:37 +0800

    feat: 补全测试用例

--- packages/eslint-plugin/rules/hooks-deps-new-line/__tests__/fixtures/valid.js
+++ packages/eslint-plugin/rules/hooks-deps-new-line/__tests__/fixtures/valid.js
@@ -2,17 +2,22 @@ useDemo(
     ()=>{}, 
     [deps]
 );
-
 useDemo(()=>{}, argument2);
+useDemo(argument1,[argument2]);
+useDemo(argument1,argument2);
 
 f(1, 2);
 f(1, [2]);
+f(()=>{},[2])
 
 var demo = useDemo(
     ()=>{}, 
     [deps]
-)
-var demo = useDemo(()=>{}, argument2)
+);
+var demo = useDemo(()=>{}, argument2);
+var demo = useDemo(argument1,[argument2]);
+var demo = useDemo(argument1,argument2);
 
-var result = f(1, 2)
-var result = f(1, [2])
+var result = f(1, 2);
+var result = f(1, [2]);
+var result = f(()=>{},[2])
