20b01a7e3be6cab26b60b9049d4e88daa74c280b
otakustay <otakustay@gmail.com>
2021-06-11 16:56:07 +0800

    refactor: 清理一些TODO
    剩下的最主要的是`webpack`没有暴露`Plugin`类型了

--- packages/config-webpack/src/partials/development.ts
+++ packages/config-webpack/src/partials/development.ts
@@ -3,8 +3,6 @@ import {ConfigurationFactory} from '../interface';
 
 const factory: ConfigurationFactory = () => {
     const config: Configuration = {
-        // TODO: 等webpack的定义更新到5.0
-        // @ts-ignore
         devtool: 'eval-cheap-module-source-map',
     };
 
