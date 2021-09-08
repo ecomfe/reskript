8e893ecfbc009b41f23f563ecddbdf712016fc9c
otakustay <otakustay@gmail.com>
2021-09-08 18:58:11 +0800

    fix(test): 非react测试也共享路径别名等配置 (#162)

--- packages/config-jest/src/preset.ts
+++ packages/config-jest/src/preset.ts
@@ -9,37 +9,36 @@ const STATIC_EXTENSIONS = [
     'mp4', 'webm', 'wav', 'mp3', 'm4a', 'aac', 'oga',
 ];
 
-const reactJestConfig = (configBasePath: string, jestConfigOptions?: JestConfigOptions) => {
+const reactJestConfig = (configBasePath: string) => {
     return {
-        moduleNameMapper: {
-            '\\.(css|less)$': resolve('identity-obj-proxy'),
-            [`\\.(${STATIC_EXTENSIONS.join('|')})$`]: `${unixify(configBasePath)}/mock`,
-            '@/(.*)$': '<rootDir>/src/$1',
-        },
         testEnvironment: 'jsdom',
         setupFiles: [`${unixify(configBasePath)}/setUp`],
         snapshotSerializers: [
             resolve('enzyme-to-json/serializer'),
         ],
-        globals: {
-            $build: {
-                mode: 'production',
-            },
-            // 如果自己有`jest.config.js`，需要自己写`global.$features`才行
-            $features: jestConfigOptions && jestConfigOptions.features || {},
-        },
         // 默认会忽略`node_modules`，所以这里要设置
         transformIgnorePatterns: [],
     };
 };
 
-// configs for node projects
 const nodeJestConfig = {
     testEnvironment: 'node',
 };
 
 export const getJestPresetConfig = (target: 'react' | 'node', configBasePath: string, options?: JestConfigOptions) => {
     const baseConfig = {
+        moduleNameMapper: {
+            '\\.(css|less)$': resolve('identity-obj-proxy'),
+            [`\\.(${STATIC_EXTENSIONS.join('|')})$`]: `${unixify(configBasePath)}/mock`,
+            '@/(.*)$': '<rootDir>/src/$1',
+        },
+        globals: {
+            $build: {
+                mode: 'production',
+            },
+            // 如果自己有`jest.config.js`，需要自己写`global.$features`才行
+            $features: options?.features ?? {},
+        },
         moduleDirectories: ['src', 'node_modules'],
         moduleFileExtensions: ['js', 'ts', 'jsx', 'tsx', 'd.ts'],
         transform: {
@@ -52,6 +51,6 @@ export const getJestPresetConfig = (target: 'react' | 'node', configBasePath: st
         collectCoverageFrom: ['<rootDir>/src/**/*.{js,jsx,ts,tsx}'],
         coveragePathIgnorePatterns: ['/node_modules/', '/__tests__/'],
     };
-    const targetConfig = target === 'react' ? reactJestConfig(configBasePath, options) : nodeJestConfig;
+    const targetConfig = target === 'react' ? reactJestConfig(configBasePath) : nodeJestConfig;
     return merge(baseConfig, targetConfig);
 };
