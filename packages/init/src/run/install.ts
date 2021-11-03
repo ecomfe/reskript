3a63a9254deba1a3eaa3a641f6c7449f4e9f473a
otakustay <otakustay@gmail.com>
2021-11-03 21:39:18 +0800

    fix(init): 把styelint版本固定在13.x (#186)

--- packages/init/src/run/install.ts
+++ packages/init/src/run/install.ts
@@ -52,7 +52,7 @@ export default async (cwd: string, options: UserOptions) => {
         ['-D'],
         [
             'eslint@7.x', // NOTE: `eslint 8.x`需要更新的Node版本，需要下个大版本更新
-            'stylelint',
+            'stylelint@13.x', // NOTE: `stylelint 14.x`是破坏性更新，需要下个大版本
             'typescript',
             'webpack',
             options.gerrit ? 'husky@4.x' : 'husky',
