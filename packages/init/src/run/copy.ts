3cfa54e521c944dd79c44181f841e5213e57ccb4
otakustay <otakustay@gmail.com>
2022-01-21 11:49:14 +0800

    fix(init): 解决init时复制文件路径错误的问题 (#239)

--- packages/init/src/run/copy.ts
+++ packages/init/src/run/copy.ts
@@ -13,10 +13,9 @@ export default async (cwd: string, options: UserOptions) => {
     const templateDirectory = path.join(__dirname, '..', '..', 'templates', 'normal-app');
     const files = await globby('**', {cwd: templateDirectory, dot: true});
     const copyFile = async (file: string) => {
-        const relative = path.relative(templateDirectory, file);
-        const destination = path.join(cwd, relative);
+        const destination = path.join(cwd, file);
         await fs.mkdir(path.dirname(destination), {recursive: true});
-        const content = await fs.readFile(file, 'utf-8');
+        const content = await fs.readFile(path.join(templateDirectory, file), 'utf-8');
         await fs.writeFile(
             destination,
             content.replace(/\{\{(\w+)\}\}/g, (match, key) => (options as any)[key].toString())
