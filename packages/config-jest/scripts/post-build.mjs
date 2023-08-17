fd975731e7cb0a66c5ff4ad45fa69821aafbcc14
otakustay <otakustay@gmail.com>
2023-08-17 15:07:24 +0800

    chore(deps): 更新依赖

--- packages/config-jest/scripts/post-build.mjs
+++ packages/config-jest/scripts/post-build.mjs
@@ -4,7 +4,7 @@ import prettier from 'prettier';
 import {dirFromImportMeta} from '@reskript/core';
 import {getJestPresetConfig} from '../dist/index.js';
 
-const dumpAsModule = (json, destination) => {
+async function dumpAsModule(json, destination) {
     // 有几个东西是用了`resolve('xxx')`变成了绝对路径，在这里要换回来再替换成`require.resolve`
     const modified = {
         ...json,
@@ -30,16 +30,13 @@ const dumpAsModule = (json, destination) => {
         trailingComma: 'es5',
         bracketSpacing: false,
     };
-    fs.writeFileSync(
-        destination,
-        // 不要问为啥自动生成的代码还要格式化，问就是强迫症。
-        prettier.format(moduleCode, prettyOptions),
-        'utf-8'
-    );
+    // 不要问为啥自动生成的代码还要格式化，问就是强迫症。
+    const output = await prettier.format(moduleCode, prettyOptions);
+    fs.writeFileSync(destination, output, 'utf-8');
 };
 
 const jestPresetConfig = target => getJestPresetConfig(target, '%RESKRIPT_NODE_MODULE_JEST_PATH%');
 const destination = path.join(dirFromImportMeta(import.meta.url), '..', 'config');
 fs.mkdirSync(destination, {recursive: true});
-dumpAsModule(jestPresetConfig('react'), path.join(destination, 'jest-react.js'));
-dumpAsModule(jestPresetConfig('node'), path.join(destination, 'jest-node.js'));
+await dumpAsModule(jestPresetConfig('react'), path.join(destination, 'jest-react.js'));
+await dumpAsModule(jestPresetConfig('node'), path.join(destination, 'jest-node.js'));
